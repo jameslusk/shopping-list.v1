23f527679cb929b31766b30aa3b6cc9787641b9d
James Lusk <jamesallenlusk@gmail.com>
2021-01-20 07:55:18 -0600

    Added checkbox element to listAppend, Added delete button to remove completed items, changed CSS styling

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userInput");
 var ul = document.querySelector("ul");
+var li = document.getElementsByTagName("li");
 
 function inputLength() {
     return input.value.length;
+}
+
+function removeParent(evt) {
+	evt.target.parentNode.remove();
+} 
+
+function underlineParent(event) {
+	event.target.parentNode.classList.toggle("done");
 }
 
 // Accepts user input and adds it to <ul>
 function createListElement() {
     var li = document.createElement("li");
     input.value = input.value.charAt(0).toUpperCase() + input.value.slice(1).toLowerCase();
+
+// Add checkbox element to list
+    var checkBox = cb = document.createElement( "input" );
+    cb.type = "checkbox";
+    cb.id = "c1";
+    cb.value = name;
+    cb.checked = false;
+//Append the checkbox to the li
+    li.appendChild(cb);
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
     input.value = "";
+// Button Element to remove list item
+    var button = document.createElement("button");
+    button.appendChild(document.createTextNode("Delete"));
+    li.appendChild(button);
+    button.onclick = removeParent;
+
 }
 
 // Accepts user input with mouse click
@@ -28,6 +52,9 @@ function addListAfterKeypress(event) {
         createListElement();
     }
 }
+
+
+
 
 button.addEventListener("click", addListAfterClick); 
 
